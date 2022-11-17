518cda36886ab474c3ccc48d6237db480169d345
York Yao <userid@bucknell.edu>
2022-11-17 15:41:58 -0800

    feature: build REST GET api

--- main.go
+++ main.go
@@ -14,11 +14,13 @@ import (
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 
-	hh := handlers.NewHello(l)
+	// hh := handlers.NewHello(l)
 	gh := handlers.NewGoodbye(l)
 
+	ph := handlers.NewProducts(l)
+
 	sm := http.NewServeMux()
-	sm.Handle("/", hh)
+	sm.Handle("/", ph)
 	sm.Handle("/goodbye", gh)
 
 	s := &http.Server{
